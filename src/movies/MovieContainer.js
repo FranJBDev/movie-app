a7d155846c816ba5feef74d2504c7aba13ef1b42
gandhyman <gandhycoder@gmail.com>
2023-01-14 00:18:03 -0700

    Add: css and movie component

--- src/movies/MovieContainer.js
+++ src/movies/MovieContainer.js
@@ -1,5 +1,7 @@
 import { movies } from '../data/movies';
 import { useState, useEffect } from 'react';
+import Movie from './Movie';
+import './movies.css';
 
 const fetchMovies = () => {
   return movies;
@@ -20,13 +22,7 @@ const MovieContainer = () => {
       <h2>Movies</h2>
       <ul className="movie-list">
         {movies.map((movie) => (
-          <li key={movie.id} className="movie">
-            <img src={movie.poster} alt={movie.title} />
-            <p>
-              {movie.title} by {movie.director} was released on {movie.year}
-            </p>
-            <p>Rating: {movie.rating}</p>
-          </li>
+          <Movie movie={movie} key={movie.id} />
         ))}
       </ul>
     </div>
